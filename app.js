8729c8f8800f3fadf942e3059c042e1cca34ef7f
Swarup <swarupc@rocketmail.com>
2021-02-28 09:30:32 +0530

    login function

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var bodyParser= require("body-parser");
+const jwt = require('jsonwebtoken');
+const port = 3000;
+const app = express();
+let User;
+
+//setting up bodyParser middleWare
+app.use(bodyParser.urlencoded({"extended":true}))
+app.use(bodyParser.json());
+
+run().catch(error => console.log(error.stack));
+//Creating Database so Can shift with example
+async function run() {
+  await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true,useUnifiedTopology:true });
+
+  await mongoose.connection.dropDatabase();
+
+  const userSchema = new mongoose.Schema({ email: String, userId: Number, password: String });
+  User = mongoose.model('UserDetail', userSchema);
+
+  await User.create({ email: 'swarup@gmail.com', userId: 1, password: '123456' });
+
+}
+//error handler middleWare
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email ,password: req.body.pwd })
+    .then(docs => {
+      console.log(docs[0].userId)
+            var token = jwt.sign({userID: docs[0].userId}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
+            res.send({token});
+        })
+        .catch(e => {
+            console.log(e);
+            next(e);
+        })
+  
+  
+  
+})
+
+app.listen(port,()=>{console.log("server started")});
